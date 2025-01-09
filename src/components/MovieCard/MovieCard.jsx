73f92f25bf3f57b40cd362d46ff634e7b1d28242
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-01-09 20:57:04 +0100

    ADD: images and style to cards

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,20 +3,21 @@ import { Link } from "react-router-dom"
 
 function MovieCard({ item = {} }) {
 
-    const { id, title, director, abstract } = item
+    const { id, image, title, director, abstract } = item
 
 
     return (
         <div className={style.card}>
-            <figure>
-                <img className={style.card_image} src="..." />
-            </figure>
-            <div className={style.card_body}>
-                <h3 className="fs-5">{title}</h3>
-                <p>{director}</p>
-                <p>{abstract}</p>
-            </div>
-            <Link className={style.movie_details} to={`/movies/${id}`}>Read more</Link>
+            <Link to={`/movies/${id}`}>
+                <figure>
+                    <img className={style.card_image} src={image} />
+                </figure>
+                <div className={style.card_body}>
+                    <h3 className="fs-5 text-center">{title}</h3>
+                    <p className="fst-italic"><span className="fst-italic">Director: </span>{director}</p>
+                    <p><span className="fst-italic">Abstract</span> <br /> {abstract}</p>
+                </div>
+            </Link>
         </div>
     )
 }
